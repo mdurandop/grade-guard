923a797c4eb78be9ad788d533c700a40c85f442a
mdurandop <mdurandop@gmail.com>
2024-06-24 20:16:22 -0500

    add modal settings and functionality

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const filaNotas = document.querySelector('.rows-notas');
 const botonAgregarNotaExtra = document.querySelector('.agregar-nota-btn');
 
-let listaNotas = [];
-let listaPorcentajes = [];
-let notaId = 1;
+let notaMinimaAprobar = 3;
+let notaMaxima = 5;
+
+const botonAjustes = document.querySelector('.js-settings-button img');
+const modalAjustes = document.querySelector('.modal-ajustes')
+
+botonAjustes.addEventListener('click', () => {
+    modalAjustes.style.display = 'block';
+
+    const notaMinPasarInput = document.querySelector('.inputs-ajustes div #nota-minima');
+    const notaMaxInput = document.querySelector('.inputs-ajustes div #nota-maxima');
+
+    const guardarCambios = document.querySelector('.modal-content button')
+
+    guardarCambios.addEventListener('click', () => {
+        notaMinimaAprobar = notaMinPasarInput.value;
+        notaMaxima = notaMaxInput.value;
+        modalAjustes.style.display = 'none';
+    });
+
+    mostrarNotasMinMax()
+
+    const botonCerrarModal = document.querySelector('.modal-content .close');
+    botonCerrarModal.addEventListener('click', () => {
+        modalAjustes.style.display = 'none';
+    });
+});
+
 
 function crearNuevaFilaNotas() {
     const nuevaFilaNotas = document.createElement('div');
@@ -11,8 +36,8 @@ function crearNuevaFilaNotas() {
 
     nuevaFilaNotas.innerHTML = 
     `
-        <input type="number" placeholder="Nota" id="nota" min="0" class="js-nota-input">
-        <input type="number" placeholder="Porcentaje" id="porcentaje" min="0">
+        <input type="number" placeholder="Nota" id="nota" min="0" max="${notaMaxima}" class="js-nota-input">
+        <input type="number" placeholder="Porcentaje" id="porcentaje" max="100" class="js-porcentaje-input" min="0">
         <img src="images/delete.svg" alt="eliminar" class="js-delete-row">
     `;
 
