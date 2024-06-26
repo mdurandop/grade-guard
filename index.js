68d40d4e51bce75366d53d55e646003c81d1e1b4
mdurandop <mdurandop@gmail.com>
2024-06-25 19:02:04 -0500

    add calculate grade function and modal

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const filaNotas = document.querySelector('.rows-notas');
+const filaNotasContenedor = document.querySelector('.rows-notas');
 const botonAgregarNotaExtra = document.querySelector('.agregar-nota-btn');
 
 let notaMinimaAprobar = 3;
 let notaMaxima = 5;
+
+const inputNota1 = document.querySelector('.first-input');
+inputNota1.setAttribute("max", `${notaMaxima}`)
 
 const botonAjustes = document.querySelector('.js-settings-button img');
 const modalAjustes = document.querySelector('.modal-ajustes')
@@ -16,12 +19,14 @@ botonAjustes.addEventListener('click', () => {
     const guardarCambios = document.querySelector('.modal-content button')
 
     guardarCambios.addEventListener('click', () => {
+        inputNota1.removeAttribute("max")
         notaMinimaAprobar = notaMinPasarInput.value;
         notaMaxima = notaMaxInput.value;
+        inputNota1.setAttribute("max", `${notaMaxima}`)
         modalAjustes.style.display = 'none';
     });
 
-    const botonCerrarModal = document.querySelector('.modal-content .close');
+    const botonCerrarModal = document.querySelector('.modal-content .close-ajustes');
     botonCerrarModal.addEventListener('click', () => {
         modalAjustes.style.display = 'none';
     });
@@ -42,24 +47,90 @@ function crearNuevaFilaNotas() {
     return nuevaFilaNotas;
 }
 
-filaNotas.addEventListener('click', (event) => {
-    // event.target selecciona el elemento en donde se realiza el event
-    // classList.contains busca si el elemento contiene una clase especifica
+filaNotasContenedor.addEventListener('click', (event) => {
     if (event.target.classList.contains('js-delete-row')) {
-        // closest busca el elemento con la clase especificada más cercana
-        // .remove elimina el elemento del DOM
         event.target.closest('.grade-guard-row').remove();
     }
 })
 
 botonAgregarNotaExtra.addEventListener('click', () => {
     const nuevaFilaNotas = crearNuevaFilaNotas();
-    filaNotas.appendChild(nuevaFilaNotas);
+    filaNotasContenedor.appendChild(nuevaFilaNotas);
 });
 
 const botonCalcularNota = document.getElementById('calcular-nota-btn');
-const resultadoText = document.querySelector('.resultado');
+const modalResultado = document.querySelector('.modal-resultado')
 
 botonCalcularNota.addEventListener('click', () => {
+    const listaNotas = [];
+    const listaPorcentajes = [];
+    const promediosParciales = [];
+    let porcentajeRestante = 0;
     
+    const filaNota = document.querySelectorAll('.grade-guard-row');
+    filaNota.forEach(fila => {
+        let nota = fila.querySelector('.js-nota-input');
+        let porcentaje = fila.querySelector('.js-porcentaje-input');
+
+        listaNotas.push(nota.value);
+        listaPorcentajes.push(porcentaje.value)
+        promediosParciales.push(nota.value * (porcentaje.value / 100))
+    })
+
+    let promedioParcial = 0;
+    let porcentajesObtenitos = 0;
+
+    listaPorcentajes.forEach(porcentaje => {
+        porcentajesObtenitos += Number(porcentaje);
+        porcentajeRestante = 100 - porcentajesObtenitos;
+    })
+
+    promediosParciales.forEach((notas) => {
+        promedioParcial += notas
+    })
+
+    let notaNecesaria = ((notaMinimaAprobar - promedioParcial) / (porcentajeRestante / 100)).toFixed(2);
+
+
+    modalResultado.style.display = 'block';
+
+    const resultadoText = modalResultado.querySelector('.modal-content');
+
+    if (notaNecesaria <= 2 && notaNecesaria > 0) {
+        resultadoText.innerHTML += 
+        `
+            <h1 class="titulo-resultado">¡Relajate!</h1>
+            <p class="mensaje-resultado">Solo necesitas un ${notaNecesaria} en tu próximo examen para pasar la materia. Estás en buen camino, ¡sigue así!</p>
+        `
+    } else if (notaNecesaria <= 3 && notaNecesaria > 2) {
+        resultadoText.innerHTML += 
+        `
+            <h1 class="titulo-resultado">¡Ya casi!</h1>
+            <p class="mensaje-resultado">Necesitas un ${notaNecesaria} en tu próximo examen para pasar la materia. Con un poco de esfuerzo y preparación, seguro lo lograrás. ¡No te rindas!</p>
+        `
+    } else if (notaNecesaria <= 4 && notaNecesaria > 3) {
+        resultadoText.innerHTML += 
+        `
+            <h1 class="titulo-resultado">¡Dale duro!</h1>
+            <p class="mensaje-resultado"> Necesitas un ${notaNecesaria} en tu próximo examen para pasar la materia. Dedica tiempo a estudiar y prepárate bien. ¡Tú puedes hacerlo!</p>
+        `  
+    } else if (notaNecesaria > 5) {
+        resultadoText.innerHTML += 
+        `
+            <h1 class="titulo-resultado">Te tenemos malas noticias</h1>
+            <p class="mensaje-resultado">Aunque has hecho un buen esfuerzo, la calificación necesaria para aprobar ${notaNecesaria}. No te desanimes; utiliza esta experiencia como una oportunidad para aprender y mejorar. ¡La próxima vez estarás mejor preparado!</p>
+        `  
+    } else {
+        resultadoText.innerHTML += 
+        `
+            <h1 class="titulo-resultado">¡Felicidades!</h1>
+            <p class="mensaje-resultado">Ya has pasado la materia con éxito. Todo tu esfuerzo ha valido la pena. ¡Sigue así y disfruta de tu logro!</p>
+        `  
+    }
+
+    const botonCerrarModal = document.querySelector('.modal-content .close-resultados');
+    botonCerrarModal.addEventListener('click', () => {
+        resultadoText.innerHTML = `<span class="close-resultados">&times;</span>`;
+        modalResultado.style.display = 'none';
+    });
 })
